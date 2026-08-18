377e386192d2b71ad57e147c5a0a9a3c53314443
Tiennv <tmed2860@gmail.com>
2026-08-18 10:36:12 +0700

    LanhNT - bai 3

--- test/specs/android-native.spec.ts
+++ test/specs/android-native.spec.ts
@@ -16,33 +16,59 @@ describe('Adnroid Native Feature Test', () => {
         await driver.startActivity("io.appium.android.apis", ".app.AlertDialogSamples")
 
         // click on first dialog
+        const theOKCancelDialogBtn = $('~OK Cancel dialog with a message')
+        await theOKCancelDialogBtn.click()
 
         // accept Alert
+        // await driver.acceptAlert()
 
         // dismiss alert
+        await driver.dismissAlert()
 
         // click on the OK button
 
         // click on the Cancel button
 
         // assertion - alert box is no longer visiable
+        const msgText = await $('android=new UiSelector().resourceId("android:id/alertTitle")')
+        await expect(msgText).not.toBeExisting()
     })
 
     it('Vertical Scrolling', async () => {
         // open Activity app: App -> Activity
+        await $('~App').click()
+        await $('~Activity').click()
 
         // scroll to end
+        // UiSelector tìm container → UiScrollable thao tác scroll → scrollToEnd() scroll đến cuối.
+        // 1 = tối đa bao nhiêu lần swipe, 5 = mỗi swipe được thực hiện với bao nhiêu step
+        // await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollToEnd(1,5)')
 
         // click on 'Secure Surfaces btn"
+        // await $('~Secure Surfaces').click()
 
         // scrollTextIntoView
+        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Secure Surfaces")')
+            .click();
 
         // assertion Secure Dialog to be Exist
+        const securityDialogBtn = $('~Secure Dialog')
+        await expect(securityDialogBtn).toBeExisting()
     })
 
     it('Horizontal Scrolling1', async () => {
         // open the target page
         // Views -> Gallery -> 1. Photos
         await driver.startActivity("io.appium.android.apis", ".view.Gallery1")
+
+        await $(
+            "android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollForward()"
+        );
+
+        await $(
+            "android=new UiScrollable(new UiSelector().scrollable(true)).setAsHorizontalList().scrollBackward()"
+        );
+
+        await driver.pause(3000);
     })
 })
