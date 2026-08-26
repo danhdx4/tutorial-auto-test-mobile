13083879d4bc0f5daadd768949db807a5a58e998
LanhNguyen <lanh.thp@gmail.com>
2026-08-26 23:02:54 +0700

    LanhNT - Prepare bai 6

---
+++ test/specs/ColorNote/add-note.spec.ts
@@ -0,0 +1,16 @@
+describe('Add Notes', () => {
+    it('Skip tutorial', async () => {
+    });
+
+    it('Should be add a new note successfully', async () => {
+        // add note, save changes, verify note
+
+        // add note title
+
+        // add note body
+
+        // save the changes
+
+        // assertion
+    })
+})
