4208e6311f4c25321a651aac1da7831a9d25f520
LanhNguyen <lanh.thp@gmail.com>
2026-09-02 16:10:48 +0700

    LanhNT - BTVN bai 6

--- test/specs/XuanNT/add-note.spec.ts
+++ test/specs/XuanNT/add-note.spec.ts
@@ -39,23 +39,25 @@ describe('Add Notes', () => {
         // navigate to edit note screen
         const editNote = $('android=new UiSelector().text("Favorite Anime")')
         await editNote.click()
-        const editBtn = $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/menu_btn")')
+        // const editBtn = $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/menu_btn")')
+        const editBtn = $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_btn")')
         await editBtn.click()
         const titleInput1 = $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_title")')
         await expect(titleInput1).toBeDisplayed()
         await titleInput1.clearValue()
         await titleInput1.addValue('Favorite Anime Edit')
-        const bodyInput1 = $('android=new UiSelector().text("OnePice\nNaruto\nGhibli")')
+        // const bodyInput1 = $('android=new UiSelector().text("OnePice\nNaruto\nGhibli")')
+        const bodyInput1 = $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_note")')
         await expect(bodyInput1).toBeDisplayed()
         await bodyInput1.clearValue()
         await bodyInput1.addValue('OnePice\nNaruto\nGhibli\nDoraemon')
 
         await driver.back() // saved
         await driver.back() // back to home
-        const targetNote1 = $('android=new UiSelector().text("Favorite Anime Edit")')   
+        const targetNote1 = $('android=new UiSelector().text("Favorite Anime Edit")')
         await expect(targetNote1).toBeDisplayed()
 
-        
-        
+
+
     })
 })
