1f3922bcb3c5477efa027e5b4d652813333c3a40
Vân Anh <vananh217.tm@gmail.com>
2026-09-02 22:41:53 +0700

    no message

--- test/specs/AnhNTV/wdio.conf.ts
+++ test/specs/AnhNTV/wdio.conf.ts
@@ -26,8 +26,7 @@ export const config: WebdriverIO.Config = {
   // of the config file unless it's absolute.
   //
   specs: [
-    './test/specs/**/*.ts'
-    
+    './*.spec.ts'
   ],
   // Patterns to exclude.
   exclude: [
