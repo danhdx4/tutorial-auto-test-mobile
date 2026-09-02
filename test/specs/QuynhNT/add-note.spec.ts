e0ffaebf950e6aa0fe45dfd49cf2c1ff103aae28
QUYNH\QUYNHNT <quynhnguyenattt@gmail.com>
2026-09-02 23:04:10 +0700

    Update QuynhNT add note test

--- test/specs/QuynhNT/add-note.spec.ts
+++ test/specs/QuynhNT/add-note.spec.ts
@@ -1,11 +1,3 @@
-const data = {
-  title: "The first note",
-  body: "Diamond\nGold\nSilver\nPlatinum",
-};
-const editData = {
-  title: "The first note1",
-  body: "Regular\nGold\nSilver\nRuby",
-};
 describe("add-note", () => {
   it("skip tutorial", async () => {
     const skipBtn = await $(
@@ -33,11 +25,11 @@ describe("add-note", () => {
     const addTitle = await $(
       'android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_title")',
     );
-    await addTitle.setValue(data.title);
+    await addTitle.setValue("The first note");
     const addBody = await $(
       'android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_note")',
     );
-    await addBody.setValue(data.body);
+    await addBody.setValue("Diamond\nGold\nSilver\nPlatinum");
     const backbtn = await $(
       'android=new UiSelector().className("android.widget.FrameLayout").instance(0)',
     );
@@ -48,14 +40,14 @@ describe("add-note", () => {
     //assertion
     await expect(addNotetext).not.toBeExisting();
     const targetNote = await $(
-      `android=new UiSelector().text("${data.title}")`,
+      'android=new UiSelector().text("The first note")',
     );
     await expect(targetNote).toBeExisting();
   });
 
   it("Edit note successfully", async () => {
     //Tại home click vào note vừa tạo
-    let targetNote = await $(`android=new UiSelector().text("${data.title}")`);
+    let targetNote = await $('android=new UiSelector().text("The first note")');
     await targetNote.click();
     const editbtn = await $(
       'android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_btn")',
@@ -64,18 +56,18 @@ describe("add-note", () => {
     //edit note
     await editbtn.click();
     const editTitle = await $(
-      `android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_title")`,
+      'android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_title")',
     );
-    await editTitle.setValue(editData.title);
+    await editTitle.setValue("The first note1");
     const editBody = await $(
-      `android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_note")`,
+      'android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/edit_note")',
     );
-    await editBody.setValue(editData.body);
+    await editBody.setValue("Regular\nGold\nSilver\nRuby");
     //save note successfully
     await driver.back(); //Save
     await driver.back(); //back về home
     //assertion
-    targetNote = await $(`android=new UiSelector().text("${editData.title}")`);
+    targetNote = await $('android=new UiSelector().text("The first note1")');
     await expect(targetNote).toBeExisting();
   });
 });
