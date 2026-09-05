77f371a7f0f09a8a27c086c54d482f0ee2a082be
DESKTOP-50E5R2J\Thuy <caongocthuy999@gmail.com>
2026-09-05 19:27:30 +0700

    ThuyCN nop btvn POM

---
+++ test/pageobjects/ThuyCN/tutorial.page.ts
@@ -0,0 +1,22 @@
+import Page from './page.js';
+
+export default class TutorialPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    public get pageTitle() {
+        return $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/textTitle")')
+    }
+
+    public get skipBtn() {
+        return $('android=new UiSelector().resourceId("com.socialnmobile.dictapps.notepad.color.note:id/btn_start_skip")')
+    }
+
+    public async waitForLoad() {
+        await expect(this.pageTitle).toBeDisplayed()
+    }
+
+    public async skipTutorial() {
+        await this.skipBtn.click()
+    }
+}
