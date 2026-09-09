e6f9d2a2ec82a8e0efb517bcb0ab2755e4701097
QUYNH\QUYNHNT <quynhnguyenattt@gmail.com>
2026-09-09 21:52:01 +0700

    Update QuynhNT page objects and specs

--- test/specs/QuynhNT/note_pom.ts
+++ test/specs/QuynhNT/note_pom.ts
@@ -1,9 +1,11 @@
 import HomePage from "../../pageobjects/QuynhNT/home.page.ts";
 import AddNotePage from "../../pageobjects/QuynhNT/note.page.ts";
 import tutorialPage from "../../pageobjects/QuynhNT/tutorial.page.ts";
+import TrashCanPage from "../../pageobjects/QuynhNT/trashcan.page.ts";
 const tutorial = new tutorialPage();
 const home = new HomePage();
 const addNote = new AddNotePage();
+const trashCan = new TrashCanPage();
 describe("Note", () => {
   before(async () => {
     await tutorial.waitForLoad();
@@ -38,5 +40,16 @@ describe("Note", () => {
     await addNote.waitForLoad();
     await addNote.deleteNote();
     await home.verifyNoteNotExist("The first note1");
+  });
+  it("Revert Note", async () => {
+    //navigate to trash can page
+    await home.btnBBmnu();
+    await home.btnTrashCan();
+    await trashCan.chooseTargetNote("The first note1");
+    await trashCan.btnTrash();
+    await driver.acceptAlert();
+    await driver.back();
+    await driver.back();
+    await home.verifyTargetNote("The first note1");
   });
 });
