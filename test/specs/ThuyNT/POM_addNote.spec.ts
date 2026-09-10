b22d3ada3e96b62c215f9d11e8f74627c555795b
ththu13 <ngothuy13102003@gmail.com>
2026-09-10 07:09:27 +0700

    ThuyNT_revert note

--- test/specs/ThuyNT/POM_addNote.spec.ts
+++ test/specs/ThuyNT/POM_addNote.spec.ts
@@ -1,11 +1,12 @@
 import HomePage from '../../pageobjects/ThuyNT/home.page.ts';
 import AddNotePage from '../../pageobjects/ThuyNT/note.page.ts';
 import TutorialPage from '../../pageobjects/ThuyNT/tutorial.page.ts';
+import TrashCanPage from '../../pageobjects/ThuyNT/trash.can.page.ts';
 import { noteData } from '../../data/note.data.ts';
 const tutorial = TutorialPage;
 const home = HomePage;
 const addNote = AddNotePage;
-
+const trashCan = TrashCanPage;
 
 describe('Note', () => {
 
@@ -48,4 +49,17 @@ describe('Note', () => {
 
         await home.verifyNoteNotExist(noteData.edit.title);
     });
+
+    it("Revert the deleted note successfully", async () => {
+        // Open the Trash Can Page
+        await home.menuHamburger.click();
+        await home.trashCan.click();
+        // Revert the note
+        await trashCan.chooseTargetNote(noteData.edit);
+        await trashCan.revertNote();
+        // Verify the note in the Home Page
+        await driver.back();
+        await driver.back();
+        await home.verifyTargetNote(noteData.edit.title);
+    })
 });
